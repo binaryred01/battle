f388b5e1fa69de4dc1da340028baae992243f469
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-06-13 17:14:26 +0100

    sets up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -97,4 +97,13 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  ENV['RACK_ENV'] = 'test'
+
+  require_relative '../app.rb'
+  require 'capybara'
+  require 'capybara/rspec'
+  require 'rspec'
+
+  Capybara.app = BattleApp
 end
